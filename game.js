b1600346dafc26fc2fdf82c3e62d3c8d31c1d205
xCranked <jensendevinm@outlook.com>
2024-03-21 17:07:25 +0300

    game.js updated with player selection; uses else if to match choices.

--- game.js
+++ game.js
@@ -1,8 +1,38 @@
-//this defines what choices there are.//
+//this prompts the player to choice rock, paper, or scissors//
 const choices = ("rock", "paper", "scissors");
 
+
+//this prompts the player to choice rock, paper, or scissors//
+function getPlayerSelection() {
+    let playerSelected = prompt(`Do you choose rock, paper, or scissors?`).toLocaleLowerCase();
+    if(playerSelected === "rock"){
+        return playerSelected;
+    } else if(playerSelected === "paper") {
+        return playerSelected;
+    } else if(playerSelected === "scissors") {
+        return playerSelected;
+    } else {
+        return "invalid!";
+    }
+}
+
+
 //This gets the computer to choice rock, paper, or scissors.//
-function computerSelection() {
-    const selection = Math.floor(Math.random(choices) * 3);
-    alert("Computer chose: " + selection);
-}computerSelection();
+function getComputerSelection() {
+    let computerSelected = Math.floor(Math.random(choices) * 3);
+    if(computerSelected = 0){
+        return "rock";
+    } else if(computerSelected = 1) {
+        return "paper";
+    } else if(computerSelected = 2) {
+        return "scissors";
+    } else {
+        return "invalid!";
+    }
+}
+
+//This alerts to browser each choice.//
+alert("Computer chose: " + getComputerSelection() + "\nYou chose: " + getPlayerSelection());
+
+
+
